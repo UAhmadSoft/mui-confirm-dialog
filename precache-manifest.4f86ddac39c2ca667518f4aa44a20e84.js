2938e174ae9d196a0fc58d44bd7f3a5f37ceee03
UAhmadSoft <umadahmad1928@gmail.com>
2021-07-23 19:56:43 +0500

    Updates

---
+++ precache-manifest.4f86ddac39c2ca667518f4aa44a20e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ce915125cae39351271224abda648d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d653a8194f20605c8a98",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "6f955027c1591a273484",
+    "url": "./static/js/2.8c92eeef.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.8c92eeef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d653a8194f20605c8a98",
+    "url": "./static/js/main.7329df99.chunk.js"
+  },
+  {
+    "revision": "79d1c185cbf5b72da52e",
+    "url": "./static/js/runtime-main.b81fd526.js"
+  }
+]);
